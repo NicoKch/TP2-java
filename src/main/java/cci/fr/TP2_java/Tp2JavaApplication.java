61dab825f015e53c17b03f198d76b6f8acb9e983
NicoKch <nkirchhoffer@gmail.com>
2026-05-06 09:18:10 +0200

    first commit

---
+++ src/main/java/cci/fr/TP2_java/Tp2JavaApplication.java
@@ -0,0 +1,13 @@
+package cci.fr.TP2_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2JavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cci/fr/TP2_java/Tp2JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package cci.fr.TP2_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
